2811c6086d228e515125816778ba7e65e37cac45
LinkWoong <Link.wong095@gmail.com>
2019-03-27 15:22:17 +0800

    Finish backend part & add sync/await implementation

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_course/models/product.dart';
-import 'package:flutter_course/scoped_models/main.dart';
 import '../widgets/ui_elements/title_default.dart';
-import 'package:scoped_model/scoped_model.dart';
 import 'dart:async';
 
 class ProductPage extends StatelessWidget {
-  final int productIndex;
-  ProductPage(this.productIndex);
+  final Product product;
+  ProductPage(this.product);
 
-  Widget _buildAddressPriceRow(double price){
+  Widget _buildAddressPriceRow(double price) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Text(
           'Union Square, San Francisco',
-          style:
-          TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
         ),
         SizedBox(
           width: 10.0,
@@ -30,8 +27,7 @@ class ProductPage extends StatelessWidget {
         ),
         Text(
           '\$' + price.toString(),
-          style:
-          TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
         )
       ],
     );
@@ -43,32 +39,35 @@ class ProductPage extends StatelessWidget {
     // However, when clicking the default return button which locates on upper left corner, the returned value would be null
     // To solve this issue, wrap the scaffold in a WillPopScope() widget.
     return WillPopScope(
-        child: ScopedModelDescendant(builder: (BuildContext context, Widget child, MainModel model){
-          final Product product = model.allProducts[productIndex];
-          return Scaffold(
-              appBar: AppBar(
-                title: Text('Product detail'),
-              ),
-              body: Column(
-                // mainAxisAlignment: MainAxisAlignment.center, // from top to bottom
-                crossAxisAlignment:
-                CrossAxisAlignment.center, // from left to right
-                children: <Widget>[
-                  Image.network(product.image),
-                  Container(
-                      padding: EdgeInsets.all(10.0), child: TitleDefault(product.title)),
-                  _buildAddressPriceRow(product.price),
-                  SizedBox(height: 10.0),
-                  Container(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Product detail'),
+            ),
+            body: Column(
+              // mainAxisAlignment: MainAxisAlignment.center, // from top to bottom
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, // from left to right
+              children: <Widget>[
+                FadeInImage(
+                  image: NetworkImage(product.image),
+                  height: 300.0,
+                  fit: BoxFit.cover,
+                  placeholder: AssetImage('assets/backgroud.jpg'),
+                ),
+                Container(
                     padding: EdgeInsets.all(10.0),
-                    child: Text(
-                      product.description,
-                      textAlign: TextAlign.center,
-                    ),
+                    child: TitleDefault(product.title)),
+                _buildAddressPriceRow(product.price),
+                SizedBox(height: 10.0),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    product.description,
+                    textAlign: TextAlign.center,
                   ),
-                ],
-              ));
-        }),
+                ),
+              ],
+            )),
         // The onWillPop argument receives a function that concerns about left upper corner return button.
         onWillPop: () {
           Navigator.pop(context, false);
